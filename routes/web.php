56860b85882664209681ea76564611dd5a698c27
Sanam Rahimi <sanamrahimi58@gmail.com>
2021-03-25 00:10:27 -0700

    updated files

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,15 @@ Route::get('/admin', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
